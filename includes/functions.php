7d87f77d56ecd46b5f799d196d65587250dbe2e7
Chieh Yang <yang@Chieh.local>
2020-11-16 00:54:45 -0500

    dev

---
+++ includes/functions.php
@@ -0,0 +1,31 @@
+<?php
+    // include the file we just wrote - connect
+     // like a JS import statement
+
+    $result = array();
+
+    function getAllUsers($conn) {
+        $query = "SELECT * FROM my_favrite";
+
+        $runQuery = $conn->query($query);
+
+        while($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
+            $result[] = $row;
+        }
+
+        //return $result;
+        echo (json_encode($result));
+    }
+
+    function getSingleUser($conn, $id) {
+        $query = "SELECT * FROM my_favrite WHERE id=" . $id . "";
+
+        $runQuery = $conn->query($query);
+
+        while($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
+            $result[] = $row;
+        }
+
+        //return $result;
+        echo (json_encode($result));
+    }
---
+++ js/modules/DataModule.js
@@ -0,0 +1,5 @@
+module.exports = {
+    name: "Trevor",
+    role: "Prof",
+    nickname: "Tervor"
+}
